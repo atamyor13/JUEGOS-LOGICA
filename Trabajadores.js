83103b0d3668134a00858544d6a871461508a253
atamyor13 <61245797+atamyor13@users.noreply.github.com>
2020-04-15 22:42:50 -0500

    Trabajadoress

---
+++ Trabajadores.js
@@ -0,0 +1,24 @@
+alert("A continuacion se presenta la lista de los trabajadores en el tiempo de 11 días para descansar 2 personas 3 horas todas las noches ");
+var an="Andres";
+var t="Thomas";
+var g="Gabriel";
+var a="Ángel";
+var s="Sofía";
+var n="Noah";
+var Sa="Samanta";
+
+
+document.write("<br> -Ellos descansaron en el día 1: "+an+","+t+ "<br>");
+document.write("<br> -Ellos descansaron en el día 2: "+g+","+a+ "<br>");
+document.write("<br> -Ellos descansaron en el día 3: "+s+","+n+ "<br>");
+document.write("<br> -Ellos descansaron en el día 4: "+Sa+","+an+ "<br>");
+document.write("<br> -Ellos descansaron en el día 5: "+t+","+n+ "<br>");
+document.write("<br> -Ellos descansaron en el día 6: "+g+","+s+ "<br>");
+document.write("<br> -Ellos descansaron en el día 7: "+a+","+an+ "<br>");
+document.write("<br> -Ellos descansaron en el día 8: "+a+","+t+ "<br>");
+document.write("<br> -Ellos descansaron en el día 9: "+g+","+n+ "<br>");
+document.write("<br> -Ellos descansaron en el día 10: "+Sa+","+g+ "<br>");
+document.write("<br> -Ellos descansaron en el día 11: "+Sa+","+s+ "<br>");
+
+document.write("<br> Los trabajadores duraron 11 días para que tods pudieran dormir de la misma manera sin repetir compañero")
+	
